40e22d701b34cd733256eb7c5d37e16550d5265b
DoneGame <d.geyzha@gmail.com>
2024-09-11 00:48:25 +0300

    reading from file, static array - functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,65 +3,20 @@
 #include <assert.h>
 #include <stdlib.h>
 
-int  my_strcmp   (const char *s1, const char *s2);
-void print_str   (char str_name[], const char *string);
-void bubble_sort (char text[50][200], unsigned index[], unsigned n_lines);
-
 const char text_filename[] = "onegin_en.txt";
+const unsigned max_lines = 50;
+const unsigned max_symbols_in_line = 200;
+
+int      my_strcmp           (const char *s1, const char *s2);
+void     print_str           (char str_name[], const char *string);
+void     bubble_sort         (char text[max_lines][max_symbols_in_line], unsigned index[], unsigned n_lines);
+unsigned read_text_from_file (const char file_name[], char text_array[max_lines][max_symbols_in_line]);
+
 
 int main () {
-    FILE *text_file = fopen (text_filename, "r");
+    char text[max_lines][max_symbols_in_line] = {};
 
-    if (!text_file) {
-        printf ("Can't open file with text!");
-        return 1;
-    }
-
-    /*
-    fseek(f, 0, SEEK_END);
-    size_t file_size = ftell(f);
-    fseek(f, 0, SEEK_SET);
-
-    const char *text = (const char *) calloc (file_size, 1);
-
-    if (!text) {
-        printf ("Failed to allocate memory!");
-        return 1;
-    }
-    */
-
-    char text[50][200] = {};
-
-    unsigned line_no = 0;
-    unsigned symbol_no = 0;
-    int c = '\0';
-    while ((c = fgetc(text_file)) != EOF) {
-        assert (line_no   < 50 );
-        assert (symbol_no < 200);
-
-        if (c == '\n') {
-            if (symbol_no == 0)
-                continue;
-
-            text[line_no++][symbol_no++] = '\0';
-            symbol_no = 0;
-        }
-        else
-            text[line_no][symbol_no++] = (char) c;
-    }
-
-    if (symbol_no == 0 && line_no > 0) {
-        line_no--;
-    }
-    else {
-        assert (line_no   < 50 );
-        assert (symbol_no < 200);
-        text[line_no][symbol_no] = '\0';
-    }
-    
-    unsigned num_lines = line_no + 1;
-
-    fclose(text_file);
+    unsigned num_lines = read_text_from_file (text_filename, text);
 
     unsigned *index = (unsigned *) calloc (num_lines, sizeof(unsigned));
 
@@ -84,7 +39,60 @@ int main () {
     return 0;
 }
 
-void bubble_sort (char text[50][200], unsigned index[], unsigned n_lines) {
+unsigned read_text_from_file (const char file_name[], char text_array[max_lines][max_symbols_in_line]) {
+    FILE *text_file = fopen (file_name, "r");
+
+    if (!text_file) {
+        printf ("Can't open file with text!");
+        return 1;
+    }
+
+    /*
+    fseek(f, 0, SEEK_END);
+    size_t file_size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    const char *text = (const char *) calloc (file_size, 1);
+
+    if (!text) {
+        printf ("Failed to allocate memory!");
+        return 1;
+    }
+    */
+
+    unsigned line_no = 0;
+    unsigned symbol_no = 0;
+    int c = '\0';
+    while ((c = fgetc(text_file)) != EOF) {
+        assert (line_no   < max_lines          );
+        assert (symbol_no < max_symbols_in_line);
+
+        if (c == '\n') {
+            if (symbol_no == 0)
+                continue;
+
+            text_array[line_no++][symbol_no++] = '\0';
+            symbol_no = 0;
+        }
+        else
+            text_array[line_no][symbol_no++] = (char) c;
+    }
+
+    if (symbol_no == 0 && line_no > 0) {
+        line_no--;
+    }
+    else {
+        assert (line_no   < max_lines          );
+        assert (symbol_no < max_symbols_in_line);
+        text_array[line_no][symbol_no] = '\0';
+    }
+
+    fclose(text_file);
+
+    return line_no + 1;
+}
+
+void bubble_sort (char text[max_lines][max_symbols_in_line], unsigned index[], unsigned n_lines) {
     for (unsigned i = n_lines; i > 0; i--) {
         for (unsigned j = 0; j < i - 1; j++) {
             assert (j + 1 < n_lines);
