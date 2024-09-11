8d613d2c7593f095c20652a352f2a6b7eeec781c
DoneGame <d.geyzha@gmail.com>
2024-09-11 22:14:41 +0300

    static pointer array, dinamic text array

--- src/main.cpp
+++ src/main.cpp
@@ -5,107 +5,112 @@
 
 const char text_filename[] = "test.txt"; // onegin_en.txt
 const unsigned max_lines = 50;
-const unsigned max_symbols_in_line = 200;
 
-int      my_strcmp           (const char *s1, const char *s2);
-void     print_str           (char str_name[], const char *string);
-void     bubble_sort         (char text[max_lines][max_symbols_in_line], unsigned index[], unsigned n_lines);
-unsigned read_text_from_file (const char file_name[], char text_array[max_lines][max_symbols_in_line]);
+struct Text {
+    char *ptr_array[max_lines];
+    unsigned num_lines;
+};
+
+void        read_from_file (const char file_name[], struct Text *readed_text);
+struct Text bubble_sort    (struct Text sorting_text);
+int         my_strcmp      (const char *s1, const char *s2);
+void        print_text     (struct Text text);
+void        print_str      (char str_name[], const char *string);
 
 
 int main () {
-    char text[max_lines][max_symbols_in_line] = {};
+    struct Text original_text = {.ptr_array = {NULL}, .num_lines = 0};
+    
+    read_from_file (text_filename, &original_text);
 
-    unsigned num_lines = read_text_from_file (text_filename, text);
-
-    unsigned *index = (unsigned *) calloc (num_lines, sizeof(unsigned));
-
-    if (!index) {
-        printf ("Failed to allocate memory!");
+    if (!original_text.num_lines)
         return 1;
-    }
 
-    for (unsigned i = 0; i < num_lines; i++)
-        index[i] = i;
+    struct Text sorted_text = bubble_sort (original_text);
 
-    bubble_sort (text, index, num_lines);
+    print_text (sorted_text);
 
-    for (unsigned i = 0; i < num_lines; i++) {
-        char str_no[10] = "";
-        itoa (i, str_no, 10);
-        print_str (str_no, text[index[i]]);
-    }
-
-    free (index); index = NULL;
+    free(original_text.ptr_array[0]); original_text.ptr_array[0] = NULL;
 
     return 0;
 }
 
-unsigned read_text_from_file (const char file_name[], char text_array[max_lines][max_symbols_in_line]) {
+void read_from_file (const char file_name[], struct Text *readed_text) {
     FILE *text_file = fopen (file_name, "r");
 
     if (!text_file) {
         printf ("Can't open file with text!");
-        return 1;
+        return;
     }
 
-    /*
-    fseek(f, 0, SEEK_END);
-    size_t file_size = ftell(f);
-    fseek(f, 0, SEEK_SET);
+    fseek(text_file, 0, SEEK_END);
+    size_t file_size = ftell(text_file);
+    fseek(text_file, 0, SEEK_SET);
 
-    const char *text = (const char *) calloc (file_size, 1);
+    char *text_array = (char *) calloc (file_size, 1);
 
-    if (!text) {
+    if (!text_array) {
         printf ("Failed to allocate memory!");
-        return 1;
+        return;
     }
-    */
 
+    readed_text->ptr_array[0] = text_array;
+
+    unsigned symbol_counter = 0;
     unsigned line_no = 0;
     unsigned symbol_no = 0;
     int c = '\0';
     while ((c = fgetc(text_file)) != EOF) {
-        assert (line_no   < max_lines          );
-        assert (symbol_no < max_symbols_in_line);
+        assert (line_no        < max_lines               );
+        assert (symbol_counter < file_size / sizeof(char));
 
         if (c == '\n') {
             if (symbol_no == 0)
                 continue;
 
-            text_array[line_no++][symbol_no++] = '\0';
+            readed_text->ptr_array[line_no][symbol_no++] = '\0';
+
+            assert (line_no + 1 < max_lines);
+            readed_text->ptr_array[line_no + 1] = readed_text->ptr_array[line_no] + symbol_no + 1;
+
+            line_no++;
+            symbol_counter++;
             symbol_no = 0;
         }
-        else
-            text_array[line_no][symbol_no++] = (char) c;
+        else {
+            symbol_counter++;
+            readed_text->ptr_array[line_no][symbol_no++] = (char) c;
+        }
     }
 
     if (symbol_no == 0 && line_no > 0) {
         line_no--;
     }
     else {
-        assert (line_no   < max_lines          );
-        assert (symbol_no < max_symbols_in_line);
-        text_array[line_no][symbol_no] = '\0';
+        assert (line_no        < max_lines               );
+        assert (symbol_counter < file_size / sizeof(char));
+        readed_text->ptr_array[line_no][symbol_no] = '\0';
     }
 
-    fclose(text_file);
+    readed_text->num_lines = line_no + 1;
 
-    return line_no + 1;
+    fclose(text_file);
 }
 
-void bubble_sort (char text[max_lines][max_symbols_in_line], unsigned index[], unsigned n_lines) {
-    for (unsigned i = n_lines; i > 0; i--) {
+struct Text bubble_sort (struct Text sorting_text) {
+    for (unsigned i = sorting_text.num_lines; i > 0; i--) {
         for (unsigned j = 0; j < i - 1; j++) {
-            assert (j + 1 < n_lines);
+            assert (j + 1 < sorting_text.num_lines);
 
-            if (my_strcmp (text[index[j]], text[index[j + 1]]) > 0) {
-                unsigned tmp = index[j];
-                index[j] = index[j + 1];
-                index[j + 1] = tmp;
+            if (my_strcmp (sorting_text.ptr_array[j], sorting_text.ptr_array[j + 1]) > 0) {
+                char *tmp = sorting_text.ptr_array[j];
+                sorting_text.ptr_array[j] = sorting_text.ptr_array[j + 1];
+                sorting_text.ptr_array[j + 1] = tmp;
             }
         }
     }
+
+    return sorting_text;
 }
 
 int my_strcmp (const char *string1, const char *string2) {
@@ -113,6 +118,14 @@ int my_strcmp (const char *string1, const char *string2) {
     for (; string1[i] != '\0' && string1[i] == string2[i]; i++) {}; 
     
     return string1[i] - string2[i];
+}
+
+void print_text (struct Text text) {
+    for (unsigned i = 0; i < text.num_lines; i++) {
+        char str_no[10] = "";
+        itoa (i, str_no, 10);
+        print_str (str_no, text.ptr_array[i]);
+    }
 }
 
 void print_str (char str_name[], const char *string) {
